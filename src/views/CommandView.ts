883362d31804f2d900fe7a6cfdfd3e4883c93384
Jason Aricheta <jason.aricheta@gmail.com>
2023-06-19 09:55:15 +1200

    working chalk integration

--- src/views/CommandView.ts
+++ src/views/CommandView.ts
@@ -1,6 +1,18 @@
+import chalk from "chalk";
 export class CommandView {
   public render(input: string) {
-    process.stdout.write(input);
+    const chalkRender = (input: string) => {
+      // process.stdout.write(input);
+      const color = {
+        lightYellow: "#fddb45",
+        lightOrange: "#f7a11b",
+        lightGreen: "#98c379",
+        lightBlue: "#61afef",
+      };
+
+      process.stdout.write(chalk.hex(color.lightBlue).bold(input));
+    };
+    chalkRender(input);
     process.stdout.write("\n");
   }
 }
--- src/views/NLView.ts
+++ src/views/NLView.ts
@@ -1,11 +1,19 @@
-// import chalk from "chalk";
+import chalk from "chalk";
 
 export class NLView {
   public async render(input: string, isToken: boolean = true) {
     // const chalk: any = (await import("chalk")).default;
     const chalkRender = (input: string) => {
-      process.stdout.write(input);
-      // return process.stdout.write(chalk.hex("#fddb45")(input));
+      // process.stdout.write(input);
+      const color = {
+        lightYellow: "#fddb45",
+        lightOrange: "#f7a11b",
+        lightGreen: "#98c379",
+        lightBlue: "#61afef",
+      };
+
+      process.stdout.write(chalk.hex(color.lightGreen).bold(input));
+      // process.stdout.write(chalk.hex("#f7a11b").bold(input));
     };
     // process.stdout.write(input);
     chalkRender(input);
